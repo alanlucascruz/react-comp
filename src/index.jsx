874d3cef7a21b2fbebbccacad8c9ad4902e72896
Alan Cruz <alanlucascruz@gmail.com>
2023-06-20 13:14:35 -0300

    .

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,15 @@
 import React, { Fragment } from "react";
 import ReactDOM from "react-dom/client";
 
-// import Sidebar from "./components/Sidebar";
-import Modal from "./components/Modal";
+import Sidebar from "./components/Sidebar";
+// import Modal from "./components/Modal";
 
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Fragment>
-    {/* <Sidebar /> */}
-    <Modal />
+    <Sidebar />
+    {/* <Modal /> */}
   </Fragment>
 );
